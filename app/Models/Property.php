9d00a9161401b7fd5398271d169ee25d5a66df46
antonkobchenko <antonkobchenko@yandex.ru>
2020-12-23 04:07:17 +0300

    Queries optimization

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Property extends Model 
+class Property extends BaseModel 
 {
     use SoftDeletes;
 
