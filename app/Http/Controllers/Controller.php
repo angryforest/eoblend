39cb8cf28ff90e6ece4f2892733ec906d95e3a88
Anton Kobchenko <rodokorn@mail.ru>
2020-12-28 04:16:12 +0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,7 +14,7 @@ class Controller extends BaseController
     public function __construct() 
     {
         $this->data = [
-            'version' => '0.26'
+            'version' => '0.27'
         ];
     }
 }
